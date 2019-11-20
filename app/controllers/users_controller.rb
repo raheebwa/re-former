6c548a69ab87af06ec4f105ffdcbc220f24ab113
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-11-20 23:12:50 +0300

    [Add] Whitelisting user parameters on submit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,21 @@
 class UsersController < ApplicationController
-    def new
+  def new
         
-    end
+  end
 
-    def create
-        
+  def create
+    @user = User.new(user_params)
+
+    if @user.save
+      redirect_to new_users_path
+    else
+      render :new
     end
+  end
+
+  private 
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password, :password_confirmation)
+  end
 end
