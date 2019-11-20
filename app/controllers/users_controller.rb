c006dea8f23d0e5438fa9bc8e898184f5cc37db2
Aheebwa Ramadhan <raheebwa@hotmail.com>
2019-11-21 01:35:44 +0300

    Added & Display of Error Messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+   before_action :set_user, only: [:edit, :update]
+
   def new
         
   end
@@ -9,13 +11,29 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to new_users_path
+      redirect_to new_user_path
     else
       render :new
     end
   end
 
+  def edit
+  
+  end
+
+  def update
+    if @user.update(user_params)
+      redirect_to new_user_path
+    else
+      render :edit
+    end
+  end
+
   private 
+
+  def set_user
+      @user = User.find(params[:id])
+  end
   
   def user_params
     params.require(:user).permit(:username, :email, :password, :password_confirmation)
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  resource :users, only: [:create, :new]
+  resources :users, only: [:create, :new, :edit, :update]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
